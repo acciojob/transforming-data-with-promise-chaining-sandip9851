e0a1da2991438ae9b61833901f0848be535be2a9
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-26 18:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", display);
+//let value = document.getElementById("ip").value;
+function display() {
+  return new Promise((resolve, reject) => {
+    let value = document.getElementById("ip").value;
+    setTimeout(() => {
+      document.getElementById("output").innerText = `Result : ${value}`;
+      resolve(value);  // you should resolve with a value
+    }, 2000)
+  }).then((value)=>{
+		return new Promise((resolve, reject) => {
+	    value = value*2
+    setTimeout(() => {
+      document.getElementById("output").innerText = `Result : ${value}`;
+      resolve(value);  // you should resolve with a value
+    }, 1000)
+	}).then((value)=>{
+		return new Promise((resolve, reject) => {
+	    value = value-3
+    setTimeout(() => {
+      document.getElementById("output").innerText = `Result : ${value}`;
+      resolve(value);  // you should resolve with a value
+    }, 1000)
+	}).then((value)=>{
+		return new Promise((resolve, reject) => {
+     value = value/2
+    setTimeout(() => {
+      document.getElementById("output").innerText = `Result : ${value}`;
+      resolve(value);  // you should resolve with a value
+    }, 1000)
+	}).then((value)=>{
+		return new Promise((resolve, reject) => {
+     value = value+10
+    setTimeout(() => {
+      document.getElementById("output").innerText = `Final Result : ${value}`;
+      resolve(value);  // you should resolve with a value
+    }, 1000)
+	})
+}
+
+  
